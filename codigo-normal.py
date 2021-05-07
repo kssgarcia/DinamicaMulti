f3b3db0e439acc1cd3fae4af4305cb90125e3c79
unknown <kssgarcia@outlook.com>
2021-05-06 20:58:20 -0500

    changes

--- codigo-normal.py
+++ codigo-normal.py
@@ -22,70 +22,77 @@ class  CierreVectorial:
         self.I2 = I2
         self.I3 = I3
         self.I4 = I4
-        self.xA = 0
-        self.yA = 0
-        self.xB = 5.908
-        self.yB = 1.0418
         self.phi2inicial = phi2inicial
         self.omega2inicial = omega2inicial
         self.alpha2inicial = alpha2inicial
         cont = 0
-        x=[.4, 1, 2.2, 2]
+        x=[3, 0, 0, 1, 0, 0, 2.4, 1.9, 1.3, 4.4, 1.9, 2.2]
         step = 0.01
         self.time_simul = time_simul
-        x1, y1, x2, y2, t = symbols('x1 y1 x2 y2 t')
+        x1, y1, phi1, x2, y2, phi2, x3, y3, phi3, x4, y4, phi4, t = symbols('x1 y1 phi1 x2 y2 phi2 x3 y3 phi3 x4 y4 phi4 t')
         w_2, w_3, w_4 = symbols('w_2 w_3 w_4')
         self.w = np.array([[w_2], [w_3], [w_4]])
-        q = np.array([[x1], [y1], [x2], [y2]])
+        q = np.array([[x1], [y1], [phi1], [x2], [y2], [phi2], [x3], [y3], [phi3], [x4], [y4], [phi4]])
 
-        phi = [[ pow((x1-self.xA),2) + pow((y1-self.yA),2) - pow(self.I2,2)],
-            [pow((x2-x1),2) + pow((y2-y1),2) - pow(self.I3,2)],
-            [pow((x2-self.xB),2) + pow((y2-self.yB),2) - pow(self.I4,2)],
-            [x1 - self.xA - self.I2*cos((0.5*alpha2inicial)*pow(t,2) + omega2inicial*t + phi2inicial)]]
+        phi = [[x1-cos(phi1)*(I1/2) - x2+cos(phi2)*(I2/2)],
+            [y1-sin(phi1)*(I1/2) - y2+sin(phi2)*(I2/2)],
+            [x2+cos(phi2)*(I2/2) - x3+cos(phi3)*(I3/2)],
+            [y2+sin(phi2)*(I2/2) - y3+sin(phi3)*(I3/2)],
+            [x3+cos(phi3)*(I3/2) - x4-cos(phi4)*(I4/2)],
+            [y3+sin(phi3)*(I3/2) - y4-sin(phi4)*(I4/2)],
+            [x1+cos(phi1)*(I1/2) - x4+cos(phi4)*(I4/2)],
+            [y1+sin(phi1)*(I1/2) - y4+sin(phi4)*(I4/2)],
+            [ x1 - (I1/2) ],
+            [ y1 ],
+            [ phi1 ],
+            [phi2-self.phi2inicial-self.omega2inicial*t-0.5*self.alpha2inicial*(pow(t,2))]]
 
 
         jaco = np.array(self.derivate(phi, q, None, 0))
         jaco_point = np.array(self.derivateMatrix(jaco, q, None, 0))
-
-
         V = []
         P = []
         A = []
         ti = []
         x = np.reshape(x, (len(x), -1))
-
+        phi = Matrix(phi)
+        jaco = Matrix(jaco)
         for i in np.arange(0, time_simul, step):
             float(i)
             cont += 1
             rest = 10
             while rest > 0.00001:
-                phiEval = Matrix(phi).subs(dict(x1=x[0][0], y1=x[1][0], x2=x[2][0], y2=x[3][0], t=i))
-                jacobian = Matrix(jaco).subs(dict(x1=x[0][0], y1=x[1][0], x2=x[2][0], y2=x[3][0], t=i))
-                phiSys = np.array(phiEval).astype(np.float64)
-                jacobianEval = np.array(jacobian).astype(np.float64)
-                xf = x - np.dot(np.linalg.inv(jacobianEval), phiSys)
+                # phiEval = phi.subs(dict(x1=x[0][0], y1=x[1][0], phi1=x[2][0], x2=x[3][0], y2=x[4][0], phi2=x[5][0], x3=x[6][0], y3=x[7][0], phi3=x[8][0], x4=x[9][0], y4=x[10][0], phi4=x[11][0], t=i))
+                # jacobian = jaco.subs(dict(x1=x[0][0], y1=x[1][0], phi1=x[2][0], x2=x[3][0], y2=x[4][0], phi2=x[5][0], x3=x[6][0], y3=x[7][0], phi3=x[8][0], x4=x[9][0], y4=x[10][0], phi4=x[11][0], t=i))
+                phiSys = np.array(phi.subs(dict(x1=x[0][0], y1=x[1][0], phi1=x[2][0], x2=x[3][0], y2=x[4][0], phi2=x[5][0], x3=x[6][0], y3=x[7][0], phi3=x[8][0], x4=x[9][0], y4=x[10][0], phi4=x[11][0], t=i))).astype(float)
+                jacobianEval = np.array(jaco.subs(dict(x1=x[0][0], y1=x[1][0], phi1=x[2][0], x2=x[3][0], y2=x[4][0], phi2=x[5][0], x3=x[6][0], y3=x[7][0], phi3=x[8][0], x4=x[9][0], y4=x[10][0], phi4=x[11][0], t=i))).astype(float)
+                xf = x - np.dot(np.linalg.solve(jacobianEval, np.identity(jacobianEval.shape[0])), phiSys)
                 x = xf
                 rest = np.linalg.norm(phiSys)
             P.append(xf)
-            v_1 = [0, 0, 0, self.I2*(self.alpha2inicial*i+self.omega2inicial)*sin(0.5*pow(self.alpha2inicial*i,2) + self.omega2inicial*i + self.phi2inicial)]
-            vi = np.dot(-np.linalg.inv(jacobianEval), np.reshape(v_1, (len(x), -1)))
+            v_1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -self.alpha2inicial*i-self.omega2inicial]
+            vi = np.dot(-np.linalg.solve(jacobianEval, np.identity(jacobianEval.shape[0])), np.reshape(v_1, (len(x), -1)))
             V.append(vi)
-            #.subs(dict(x1=x[0][0], x2=x[1][0], x3=x[2][0], x4=x[3][0], w_2=vi[0][0], w_3=vi[1][0], w_4=vi[2][0]))
             jacobina_point = Matrix(jaco_point)
-            a_1 = [0, 0, 0, ((self.I2*self.alpha2inicial*sin(0.5*pow(self.alpha2inicial*i,2) + self.omega2inicial*i + self.phi2inicial)) + self.I2*pow((self.alpha2inicial*i+self.omega2inicial),2)*cos(0.5*pow((self.alpha2inicial*i),2) + self.omega2inicial*i + self.phi2inicial))]
-            ai = np.dot(-np.linalg.inv(jacobianEval),((jacobina_point*vi)+np.reshape(a_1, (len(x), -1))))
+            a_1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -self.alpha2inicial]
+            ai = np.dot(np.linalg.solve(jacobianEval, np.identity(jacobianEval.shape[0])),(-jacobina_point*vi-np.reshape(a_1, (len(x), -1))))
             A.append(ai)
 
-
-            self.barra1.setData([self.xA, xf[0][0]], [self.yA, xf[1][0]])
-            self.barra2.setData([xf[0][0], xf[2][0]], [xf[1][0], xf[3][0]])
-            self.barra3.setData([xf[2][0], self.xB], [xf[3][0], self.yB])
-            self.barra4.setData([self.xB, self.xA], [self.yB, self.yA])
+            br1_x = float(self.I1*cos(xf[3][0]))
+            br1_y = float(self.I1*sin(xf[3][0]))
+            self.barra1.setData([0, br1_x], [0, br1_y])
+            br2_x = float(br1_x+self.I3*cos(xf[6][0]))
+            br2_y = float(br1_y+self.I3*sin(xf[6][0]))
+            self.barra2.setData([br1_x, br2_x], [br1_y, br2_y])
+            br3_x = float(self.I1*cos(xf[9][0]))
+            br3_y = float(self.I1*sin(xf[9][0]))
+            self.barra3.setData([br2_x, br3_x], [br2_y, br3_y])
+            self.barra4.setData([br3_x, 0], [br3_y, 0])
             if i >= time_simul:
                 break
             app.processEvents()
+        print(A)
 
-    
 
     def derivate(self, functionOver, derivationVar, container=None, initIter=0):
         if container == None:
@@ -120,5 +127,5 @@ if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         object_1 =  CierreVectorial()
-        object_1.SolucionVectorial(6, 2, 4, 5, 0, 1, 1, 1.5)
+        object_1.SolucionVectorial(6, 2, 4, 5, 0, 1, 1, 2.5)
         QtGui.QApplication.instance().exec_()
--- test.py
+++ test.py
@@ -1,21 +1,53 @@
-from pyqtgraph.Qt import QtGui, QtCore, QtWidgets
 import numpy as np
-import pyqtgraph as pg
-from sympy import symbols, evalf, diff, sin, cos, Matrix
-from PyQt5.QtCore import QThread, pyqtSignal
-import time 
+A = np.random.rand(1000,3,3)
+def slow_inverse(A): 
 
-class Coordenadas(QThread):
-    data = pyqtSignal(int)
-    def __init__(self, n, time, parent=None):
-        QThread.__init__(self, parent)
-        self.n = n
-        self.time = time
+    for i in range(900):
+        a = np.linalg.inv(A[i])
+    return 'listo'
 
-    def run(self):
-        for i in range(self.time):
-            a = i*2
-            time.sleep(2)
-            self.data.emit(self.n)
+def fast_inverse(A):
+    identity = np.identity(A.shape[0], dtype=A.dtype)
+    Ainv = np.zeros_like(A)
 
-            
+    for i in range(A.shape[3]):
+        Ainv[i] = np.linalg.solve(A[i], identity)
+    return Ainv
+
+def fast_inverse2(A):
+    identity = np.identity(A.shape[2], dtype=A.dtype)
+    for i in range(900):
+        a = np.linalg.solve(A[i], identity)
+    return 'listo'
+
+from numpy.linalg import lapack_lite
+lapack_routine = lapack_lite
+
+def faster_inverse(A):
+    b = np.identity(A.shape[2], dtype=A.dtype)
+
+    n_eq = A.shape[1]
+    n_rhs = A.shape[2]
+    pivots = zeros(n_eq, np.intc)
+    identity  = np.eye(n_eq)
+    def lapack_inverse(a):
+        b = np.copy(identity)
+        pivots = zeros(n_eq, np.intc)
+        results = lapack_lite.dgesv(n_eq, n_rhs, a, n_eq, pivots, b, n_eq, 0)
+        if results['info'] > 0:
+            raise LinAlgError('Singular matrix')
+        return b
+
+    return array([lapack_inverse(a) for a in A])
+
+
+B = np.array([[1,2,3],
+            [1,2,7],
+            [1,7,3]])
+# print(np.linalg.inv(B))
+# print(np.linalg.solve(B, np.identity(B.shape[0], dtype=A.dtype)))
+
+
+# print(slow_inverse(A))
+print(fast_inverse2(A))
+
